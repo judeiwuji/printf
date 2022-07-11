67c43298ed9cc9aa7a17d22dd2ef90ae0a772d2f
Ifiok Ekott <ekottifiok@gmail.com>
2022-07-11 20:19:31 +0100

    started working on custom specifier

---
+++ print_custom_specifier.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_string - prints a string
+ * @n: The index of the arg in the list
+ * @format: The format for this arg
+ * @args: The argument list
+ *
+ * Return: (int) The number of characters printed
+ */
+int print_string(int n, char *format, va_list args)
+{
+	char *str = format;
+	int count = 0;
+
+	for (; n > 0; n--)
+		va_arg(args, void *);
+
+	str = va_arg(args, char *);
+
+	if (str == NULL)
+		str = "(null)";
+
+	while (*str != '\0')
+		if (*str == '\\' && *str++ != '\0')
+			*str = '\\';
+			*str++ = 'x';
+		count += _putchar(*str++);
+	count += left_align(format, count);
+	return (count);
+}
